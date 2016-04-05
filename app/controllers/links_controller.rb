3a0e9709ca8e1e82d683e7707f60864f6c823e81
Kurt Friedrich <kurt.is.friedrich@gmail.com>
2016-04-05 17:32:31 -0400

    Remove votes from links strong parameters. Fixes #3

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -71,6 +71,6 @@ class LinksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def link_params
-      params.require(:link).permit(:url, :votes, :user)
+      params.require(:link).permit(:url, :user)
     end
 end
